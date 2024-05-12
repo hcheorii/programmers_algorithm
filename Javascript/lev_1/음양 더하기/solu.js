fe5277e72931238cf84cc98edcc0b51c5e25ae65
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-05-12 23:41:16 +0900

    문제풀이 업로드

---
+++ Javascript/lev_1/음양 더하기/solu.js
@@ -0,0 +1,11 @@
+function solution(absolutes, signs) {
+    var sum = 0;
+    for (var i = 0; i < signs.length; i++) {
+        if (signs[i] == true) {
+            sum += absolutes[i];
+        } else {
+            sum -= absolutes[i];
+        }
+    }
+    return sum;
+}
