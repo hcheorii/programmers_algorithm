0bf917250853bacbc20b75fd867b8dc448cd51bc
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-05-15 23:43:59 +0900

    문제풀이 업로드

---
+++ Javascript/lev_1/없는 숫자 더하기/solu.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    var tmp = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var sum = 0;
+    tmp.forEach((item) => {
+        numbers.indexOf(item) == -1 ? (sum += item) : null;
+    });
+    return sum;
+}
+function solution(numbers) {
+    var tmp = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var sum = 0;
+    tmp.forEach((item) => {
+        numbers.indexOf(item) == -1 ? (sum += item) : null;
+    });
+    return sum;
+}
